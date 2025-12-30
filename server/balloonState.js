99d51e8ac50af5b63976e80a34bb311581a943ac
angelsolares <angelsolares@gmail.com>
2025-12-29 23:31:44 -0700

    Change ballon confif

--- server/balloonState.js
+++ server/balloonState.js
@@ -5,8 +5,8 @@
 
 const BALLOON_CONFIG = {
     TARGET_SIZE: 100,       // UI target
-    INFLATE_AMOUNT: 2,      // A bit more air to compensate for fast deflation
-    DEFLATE_RATE: 4.0,     // Much faster deflation (difficulty)
+    INFLATE_AMOUNT: 1,      // A bit more air to compensate for fast deflation
+    DEFLATE_RATE: 2.0,     // Much faster deflation (difficulty)
     COOLDOWN: 50,           // 50ms cooldown
     GAME_DURATION: 60,      // Seconds
 };
